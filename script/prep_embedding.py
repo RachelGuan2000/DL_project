dd7528a51ca227a1d2e14a708644ab55003720b1
Rui Guan <rui.guan@yale.edu>
2023-04-25 17:56:56 -0400

    format some files

--- script/prep_embedding.py
+++ script/prep_embedding.py
@@ -1,25 +1,27 @@
 import argparse
 import numpy as np
 import json
-import subprocess
 
 def gen_np_embedding(fn, word_idx_fn, out_fn, dim=300):
+
     with open(word_idx_fn) as f:
-        word_idx=json.load(f)
-    embedding=np.zeros((len(word_idx)+2, dim) )
+        word_idx = json.load(f)
+
+    embedding = np.zeros((len(word_idx)+2, dim))
     with open(fn) as f:
         for l in f:
-            rec=l.rstrip().split(' ')
-            if len(rec)==2: #skip the first line.
-                continue 
+            rec = l.rstrip().split(' ')
+            if len(rec) == 2:
+                continue
             if rec[0] in word_idx:
-                embedding[word_idx[rec[0]]]=np.array([float(r) for r in rec[1:] ])
+                embedding[word_idx[rec[0]]] = np.array([float(r) for r in rec[1:] ])
     with open(out_fn+".oov.txt", "w") as fw:
         for w in word_idx:
-            if embedding[word_idx[w] ].sum()==0.:
+            if embedding[word_idx[w] ].sum() == 0.:
                 fw.write(w+"\n")
     np.save(out_fn+".npy", embedding.astype('float32') )
-    
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--emb_dir', type=str, default="data/embedding/")
 parser.add_argument('--out_dir', type=str, default="data/prep_data/")
--- script/train.py
+++ script/train.py
@@ -13,22 +13,23 @@ torch.cuda.manual_seed(1337)
 
 def batch_generator(X, y, batch_size=128, return_idx=False, crf=False):
     for offset in range(0, X.shape[0], batch_size):
-        batch_X_len=np.sum(X[offset:offset+batch_size]!=0, axis=1)
-        batch_idx=batch_X_len.argsort()[::-1]
-        batch_X_len=batch_X_len[batch_idx]
-        batch_X_mask=(X[offset:offset+batch_size]!=0)[batch_idx].astype(np.uint8)
-        batch_X=X[offset:offset+batch_size][batch_idx] 
-        batch_y=y[offset:offset+batch_size][batch_idx]
+        batch_X_len = np.sum(X[offset:offset + batch_size]!=0, axis=1)
+        batch_idx = batch_X_len.argsort()[::-1]
+        batch_X_len = batch_X_len[batch_idx]
+        batch_X_mask = (X[offset:offset + batch_size]!=0)[batch_idx].astype(np.uint8)
+        batch_X = X[offset:offset+batch_size][batch_idx]
+        batch_y = y[offset:offset+batch_size][batch_idx]
         batch_X = torch.autograd.Variable(torch.from_numpy(batch_X).long().cuda() )
-        batch_X_mask=torch.autograd.Variable(torch.from_numpy(batch_X_mask).long().cuda() )
+        batch_X_mask = torch.autograd.Variable(torch.from_numpy(batch_X_mask).long().cuda() )
         batch_y = torch.autograd.Variable(torch.from_numpy(batch_y).long().cuda() )
-        if len(batch_y.size() )==2 and not crf:
-            batch_y=torch.nn.utils.rnn.pack_padded_sequence(batch_y, batch_X_len, batch_first=True)
-        if return_idx: #in testing, need to sort back.
+        if len(batch_y.size()) == 2 and not crf:
+            batch_y = torch.nn.utils.rnn.pack_padded_sequence(batch_y, batch_X_len, batch_first=True)
+        if return_idx:
+            #in testing, need to sort back.
             yield (batch_X, batch_y, batch_X_len, batch_X_mask, batch_idx)
         else:
             yield (batch_X, batch_y, batch_X_len, batch_X_mask)
-            
+
 class Model(torch.nn.Module):
     def __init__(self, gen_emb, domain_emb, num_classes=3, dropout=0.5, crf=False):
         super(Model, self).__init__()
@@ -36,7 +37,7 @@ class Model(torch.nn.Module):
         self.gen_embedding.weight=torch.nn.Parameter(torch.from_numpy(gen_emb), requires_grad=False)
         self.domain_embedding = torch.nn.Embedding(domain_emb.shape[0], domain_emb.shape[1])
         self.domain_embedding.weight=torch.nn.Parameter(torch.from_numpy(domain_emb), requires_grad=False)
-    
+
         self.conv1=torch.nn.Conv1d(gen_emb.shape[1]+domain_emb.shape[1], 128, 5, padding=2 )
         self.conv2=torch.nn.Conv1d(gen_emb.shape[1]+domain_emb.shape[1], 128, 3, padding=1 )
         self.dropout=torch.nn.Dropout(dropout)
@@ -48,8 +49,8 @@ class Model(torch.nn.Module):
         self.crf_flag=crf
         if self.crf_flag:
             from allennlp.modules import ConditionalRandomField
-            self.crf=ConditionalRandomField(num_classes)            
-          
+            self.crf=ConditionalRandomField(num_classes)
+
     def forward(self, x, x_len, x_mask, x_tag=None, testing=False):
         x_emb=torch.cat((self.gen_embedding(x), self.domain_embedding(x) ), dim=2)
         x_emb=self.dropout(x_emb).transpose(1, 2)
@@ -87,7 +88,7 @@ def valid_loss(model, valid_X, valid_y, crf=False):
     return sum(losses)/len(losses)
 
 def train(train_X, train_y, valid_X, valid_y, model, model_fn, optimizer, parameters, epochs=200, batch_size=128, crf=False):
-    best_loss=float("inf") 
+    best_loss=float("inf")
     valid_history=[]
     train_history=[]
     for epoch in range(epochs):
@@ -101,21 +102,21 @@ def train(train_X, train_y, valid_X, valid_y, model, model_fn, optimizer, parame
         loss=valid_loss(model, train_X, train_y, crf=crf)
         train_history.append(loss)
         loss=valid_loss(model, valid_X, valid_y, crf=crf)
-        valid_history.append(loss)        
+        valid_history.append(loss)
         if loss<best_loss:
             best_loss=loss
             torch.save(model, model_fn)
         shuffle_idx=np.random.permutation(len(train_X) )
         train_X=train_X[shuffle_idx]
         train_y=train_y[shuffle_idx]
-    model=torch.load(model_fn) 
+    model=torch.load(model_fn)
     return train_history, valid_history
 
 def run(domain, data_dir, model_dir, valid_split, runs, epochs, lr, dropout, batch_size=128):
     gen_emb=np.load(data_dir+"gen.vec.npy")
     domain_emb=np.load(data_dir+domain+"_emb.vec.npy")
     ae_data=np.load(data_dir+domain+".npz")
-    
+
     valid_X=ae_data['train_X'][-valid_split:]
     valid_y=ae_data['train_y'][-valid_split:]
     train_X=ae_data['train_X'][:-valid_split]
@@ -133,15 +134,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_dir', type=str, default="model/")
     parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--epochs', type=int, default=200) 
+    parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--runs', type=int, default=5)
     parser.add_argument('--domain', type=str, default="laptop")
     parser.add_argument('--data_dir', type=str, default="data/prep_data/")
     parser.add_argument('--valid', type=int, default=150) #number of validation data.
-    parser.add_argument('--lr', type=float, default=0.0001) 
-    parser.add_argument('--dropout', type=float, default=0.55) 
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--dropout', type=float, default=0.55)
 
     args = parser.parse_args()
 
     run(args.domain, args.data_dir, args.model_dir, args.valid, args.runs, args.epochs, args.lr, args.dropout, args.batch_size)
-
